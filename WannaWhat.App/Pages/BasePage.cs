6667edbc9e8dd5fad8230d3b4bc5f98b8d0dd0ef
hloni2010 <hloni2010@gmail.com>
2020-12-20 23:54:23 +0200

    Added missing pages

---
+++ WannaWhat.App/Pages/BasePage.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WannaWhat.App.Pages
+{
+    public class BasePage : ComponentBase
+    {
+        [Parameter]
+        public string Heading { get; set; }
+
+        [Parameter]
+        public string Description { get; set; }
+    }
+}
